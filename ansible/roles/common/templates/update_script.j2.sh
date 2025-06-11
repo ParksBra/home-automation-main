ec0bd06e60c40c69d503bbcddcaa020bff98e2e5
Brandon <brandon@parksbra.com>
2025-06-10 21:54:01 -0700

    update scripts and placement

--- ansible/roles/common/templates/update_script.j2.sh
+++ ansible/roles/common/templates/update_script.j2.sh
@@ -1,7 +1,9 @@
 apt-get update -y
 apt full-upgrade -y
-cd /home/brand/smartboi/docker
+cd {{ docker_compose_path }}
 docker compose pull
 docker compose up -d
 sleep 20
+docker compose down
+sleep 5
 shutdown -r now
